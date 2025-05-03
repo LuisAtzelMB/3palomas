52f85668c4fa77faefb2bdb53b76f95b1ca8b507
LuisAtzelMB <luisatzelmb@gmail.com>
2025-05-02 18:43:20 -0600

    segundo commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useRef } from "react";
-import { PDFDocument, rgb, PDFPage } from "pdf-lib";
+import { PDFDocument, rgb } from "pdf-lib";
 
 const PdfCheckmarkAdder = () => {
   const [inputFile, setInputFile] = useState<File | null>(null);
